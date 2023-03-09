1bf574023c46126c25ced2638fc3cbe8f82200eb
Pavel-dnipro <56679738+Pavel-dnipro@users.noreply.github.com>
2023-03-09 08:53:09 +0200

    aded network service

--- lib/api/network_service.dart
+++ lib/api/network_service.dart
@@ -1,1 +1,9 @@
-class NetworkService {}
+import 'package:http/http.dart' as http;
+
+class NetworkService {
+  Future<http.Response> get(String url) async {
+    final response = await http.get(Uri.parse(url));
+
+    return response;
+  }
+}
--- lib/repositories/network_jokes_repository.dart
+++ lib/repositories/network_jokes_repository.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:jocker/api/network_service.dart';
 import 'package:jocker/business_objects/joke.dart';
 import 'package:jocker/repositories/jokes_repository.dart';
-import 'package:http/http.dart';
 
 class NetworkJokesRepository extends JokesRepository {
   NetworkJokesRepository(this.networkService);
@@ -12,8 +11,8 @@ class NetworkJokesRepository extends JokesRepository {
 
   @override
   Future<Joke> getJoke() async {
-    final response =
-        await get(Uri.parse('https://api.chucknorris.io/jokes/random'));
+    String endpoint = 'https://api.chucknorris.io/jokes/random';
+    final response = await networkService.get(endpoint);
     final joke = Joke.fromJson(jsonDecode(response.body));
 
     return joke;
